f908eb26e687b94c6f781b6dec6a0fba5740c598
Danae Graziano <oliverbakic93@gmail.com>
2026-06-23 20:27:31 +0200

    Encryption rigorous tests passed

--- caesar.py
+++ caesar.py
@@ -5,22 +5,29 @@ alphabet_uc = string.ascii_uppercase*2
 def caesar_encrypt(plaintext, shift):
     # implement the caesar cipher encryption logic here
     ciphertext = ""
+    shift %= 26
     for char in plaintext:
-        # Switch each LETTER and transpose to new letter 
-        # depending on what step was chosen
+        # switch each LETTER and encrypt to new letter 
+        # based on shift
         if char in alphabet_lc:
             position = alphabet_lc.index(char) + shift
             ciphertext += alphabet_lc[position]
         elif char in alphabet_uc:
+            position = alphabet_uc.index(char) + shift
             ciphertext += alphabet_uc[position]
         else:
-        # this is for punctuation
+        # this is for special chars and emptyspaces
             ciphertext += char
 
     print(ciphertext)
     return ciphertext
-def caesar_decrypt():
+def caesar_decrypt(ciphertext, shift):
     # implement the caesar cipher decryption logic here
+    return 0
+
+def caesar_bruteforce(ciphertext):
+    # implement a bruteforce that goes through every 
+    # possible shift and returns them all in a list
     return 0
 
 caesar_encrypt("hello", -2)
