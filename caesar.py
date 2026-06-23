e932a3e8a4306d977ce7fedc856fd4904cb51f35
Danae Graziano <oliverbakic93@gmail.com>
2026-06-23 20:02:42 +0200

    first version of encryption

--- caesar.py
+++ caesar.py
@@ -1,7 +1,26 @@
-def caesar_encrypt():
+import string
+alphabet_lc = string.ascii_lowercase*2
+alphabet_uc = string.ascii_uppercase*2
+
+def caesar_encrypt(plaintext, shift):
     # implement the caesar cipher encryption logic here
-    return 0
+    ciphertext = ""
+    for char in plaintext:
+        # Switch each LETTER and transpose to new letter 
+        # depending on what step was chosen
+        if char in alphabet_lc:
+            position = alphabet_lc.index(char) + shift
+            ciphertext += alphabet_lc[position]
+        elif char in alphabet_uc:
+            ciphertext += alphabet_uc[position]
+        else:
+        # this is for punctuation
+            ciphertext += char
+
+    print(ciphertext)
+    return ciphertext
 def caesar_decrypt():
     # implement the caesar cipher decryption logic here
     return 0
 
+caesar_encrypt("hello", -2)
--- tests/test_ciphers.py
+++ tests/test_ciphers.py
@@ -1,23 +1,19 @@
-# Ciphers/main tests
 import caesar
 
+# Caesar cipher tests
 
 def test_caesar_encrypt_function_exists():
     assert hasattr(caesar, "caesar_encrypt")
 
-
 def test_caesar_decrypt_function_exists():
     assert hasattr(caesar, "caesar_decrypt")
 
+from caesar import caesar_encrypt, caesar_decrypt
 
-# Caesar cipher tests
-
-# from caesar import caesar_encrypt, caesar_decrypt
-
-# def test_caesar_encrypt():
-#     assert caesar_encrypt('abc', 1) == 'bcd'
-#     assert caesar_encrypt('xyz', 3) == 'abc'
-#     assert caesar_encrypt("hello world!", 1) == "ifmmp xpsme!"
+def test_caesar_encrypt():
+    assert caesar_encrypt("abc", 1) == "bcd"
+    assert caesar_encrypt("xyz", 3) == "abc"
+    assert caesar_encrypt("hello world!", 1) == "ifmmp xpsme!"
 
 # def test_caesar_decrypt():
 #     assert caesar_decrypt('bcd', 1) == 'abc'
